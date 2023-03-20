60d33b95f3f804adaff5ce7f7037c4ae356efc3b
manindra <manindratoyou@gmail.com>
2023-03-21 00:38:40 +0530

    Initial commit

---
+++ SpringBootPracticeGithub/SpringBootPracticeGithub/src/test/java/com/manindra/SpringBootPracticeGithub/SpringBootPracticeGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manindra.SpringBootPracticeGithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
